042928a30ad64efceab7fda1c8da9a6878de6751
aburiad <aburiad507@gmail.com>
2024-10-03 00:18:29 +0600

    added Display which page displaying Popup

--- admin/admin-ui.php
+++ admin/admin-ui.php
@@ -14,11 +14,34 @@
                 <input
                         type="url"
                         name="video_url"
-                        value="<?php echo esc_attr($options['video_url']);?>"
+                        value="<?php echo esc_attr($options['video_url']); ?>"
                         id="video_url"
                         placeholder="Video URL"
                         class="formbold-form-input"
                 />
+            </div>
+            <div class="formbold-mb-5 radio-input">
+                <label class="formbold-form-label" for="homepage">Display Page</label>
+                <div>
+                    <span>Home Page</span>
+                    <input type="radio" name="selectpage" value="homepage" id="homepage"/>
+                </div>
+                <div>
+                    <span>Enter Website</span>
+                    <input type="radio" name="seleectpage" value="enterwebsite" id="enterwebsite"/>
+                </div>
+                <div>
+                    <span>Post Only</span>
+                    <input type="radio" name="seleectpage" value="post" id="post"/>
+                </div>
+                <div>
+                    <span>Page Only</span>
+                    <input type="radio" name="seleectpage" value="page" id="page"/>
+                </div>
+                <div>
+                    <span>Specefic Page</span>
+                    <input type="radio" name="seleectpage" value="specefic" id="specefic"/>
+                </div>
             </div>
             <button type="submit" name="submit" class="formbold-btn">Submit Data</button>
     </div>
@@ -30,9 +53,10 @@
 
 if (isset($_POST['submit']) && check_admin_referer('pp_notification_nonce_action', 'pp_notification_nonce')) {
     $video_url = isset($_POST['video_url']) ? sanitize_text_field(wp_unslash($_POST['video_url'])) : '';
-
+    $seleectpage = isset($_POST['seleectpage']) ? sanitize_text_field(wp_unslash($_POST['seleectpage'])) : '';
 
     $options['video_url'] = $video_url;
+    $options['seleectpage'] = $seleectpage;
 
     // Update the option in the database
     $encoded_data = serialize($options); // Use serialize instead of json_encode
