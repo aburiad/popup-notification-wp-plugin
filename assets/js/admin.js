38461bca0144f7eb6c0088e8bb2b397031c7344b
aburiad <aburiad507@gmail.com>
2024-10-24 00:31:35 +0600

    remove: unnessary js comment

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -19,52 +19,16 @@ jQuery(document).ready(function () {
         width: 'resolve'
     });
 
-    // jQuery('.js-example-basic-multiple').on('input', function () {
-    //     var searchQuery = jQuery(this).val();
-    //
-    //     if (searchQuery.length > 2) { // Only search if input has more than 2 characters
-    //         jQuery.ajax({
-    //             url: myAjax.ajaxurl, // Use the localized ajax URL
-    //             type: 'POST',
-    //             data: {
-    //                 action: 'my_action', // AJAX action name defined in PHP
-    //                 search_query: searchQuery,
-    //             },
-    //             success: function (response) {
-    //                 console.log(response); // Log the full response
-    //
-    //                 if (response.success) {
-    //                     var resultHtml = '';
-    //                     jQuery.each(response.data, function(index, post) {
-    //                         console.log(post); // Log each post
-    //                         resultHtml += '<li><a href="' + post.link + '">' + post.title + '</a></li>';
-    //                     });
-    //
-    //                     console.log(resultHtml); // Log the generated HTML
-    //                     jQuery('#search-results').html('<ul>' + resultHtml + '</ul>');
-    //                 } else {
-    //                     jQuery('#search-results').html('<p>' + response.data + '</p>');
-    //                 }
-    //             },
-    //             error: function(jqXHR, textStatus, errorThrown) {
-    //                 console.log('AJAX error:', textStatus, errorThrown); // Log AJAX errors
-    //             }
-    //         });
-    //     } else {
-    //         jQuery('#search-results').html(''); // Clear results if input is less than 2 characters
-    //     }
-    // });
-
     jQuery('.js-example-basic-multiple').select2({
         ajax: {
-            url: myAjax.ajaxurl, // Use the localized ajax URL
+            url: myAjax.ajaxurl,
             type: 'POST',
-            dataType: 'json', // Expecting JSON response from the server
-            delay: 250, // Delay the request to prevent unnecessary rapid requests
+            dataType: 'json',
+            delay: 250,
             data: function(params) {
                 return {
-                    action: 'my_action', // The action name to call in PHP
-                    search_query: params.term, // The search query entered by the user
+                    action: 'my_action',
+                    search_query: params.term,
                 };
             },
             processResults: function(response) {
@@ -72,8 +36,8 @@ jQuery(document).ready(function () {
                     return {
                         results: response.data.map(function(post) {
                             return {
-                                id: post.link, // ID or value you want to return
-                                text: post.title // Displayed text
+                                id: post.link,
+                                text: post.title
                             };
                         })
                     };
@@ -83,9 +47,9 @@ jQuery(document).ready(function () {
                     };
                 }
             },
-            cache: true // Enable caching to minimize requests
+            cache: true
         },
-        minimumInputLength: 3 // Minimum characters required before search starts
+        minimumInputLength: 3
     });
 
 
